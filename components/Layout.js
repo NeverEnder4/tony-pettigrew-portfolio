18f3095b10b10cca9839be9b174e26c0c842aecd
NeverEnder4 <apettigrew.wsdev@gmail.com>
2018-12-09 13:31:43 -0800

    Fixed a bug where the header containing the nav elements would show at the bottom of the page on page load, fixed by using styled-jsx/css variables to change the display depending on the loading state

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,21 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import css from 'styled-jsx/css';
 
 import Loading from '../components/Loading/Loading';
-
 import SVGLogo from './SVGLogo/SVGLogo';
-import { drawSVG } from './SVGLogo/animations';
+
+const visible = css`
+  .visibility {
+    display: flex;
+  }
+`;
+
+const hidden = css`
+  .visibility {
+    display: none;
+  }
+`;
 
 class Layout extends React.Component {
   constructor() {
@@ -39,18 +50,18 @@ class Layout extends React.Component {
     const loadingComponent = loading ? (
       <Loading transition={transition} />
     ) : null;
-
+    const showHeader = loading ? hidden : visible;
     return (
       <div>
         <Head>
           <title>{title}</title>
         </Head>
         {loadingComponent}
-        <header>
+        <header className="visibility">
           <nav>
             <Link prefetch href="/">
               <a>
-                <SVGLogo animation={drawSVG} id={'logo'} />
+                <SVGLogo />
                 <p className="color-grey"> tonypettigrew.com</p>
               </a>
             </Link>
@@ -78,72 +89,77 @@ class Layout extends React.Component {
         </header>
 
         {loading || children}
-        <style jsx>{`
-          header {
-            background: #131313;
-            display: flex;
-            justify-content: center;
-          }
-          nav {
-            display: flex;
-            justify-content: space-between;
-            padding: 0.5em 0;
-            width: 86vw;
-          }
-          nav a {
-            text-decoration: none;
-            font-size: 1.5rem;
-            transition: color 0.15s ease-out;
-            display: flex;
-            align-items: center;
-          }
+        <style jsx>{showHeader}</style>
+        <style jsx>
+          {`
+            header {
+              background: #131313;
+              display: flex;
+              justify-content: center;
+              z-index: 10;
+            }
 
-          nav a img {
-            transition: opacity 0.1s ease-out;
-          }
+            nav {
+              display: flex;
+              justify-content: space-between;
+              padding: 0.5em 0;
+              width: 86vw;
+            }
+            nav a {
+              text-decoration: none;
+              font-size: 1.5rem;
+              transition: color 0.15s ease-out;
+              display: flex;
+              align-items: center;
+            }
 
-          nav a:hover,
-          nav a:focus {
-            color: #565656;
-          }
+            nav a img {
+              transition: opacity 0.1s ease-out;
+            }
 
-          nav a:hover img,
-          nav a:focus img {
-            opacity: 0.3;
-          }
+            nav a:hover,
+            nav a:focus {
+              color: #565656;
+            }
 
-          .color-yellow img {
-            height: 1em;
-          }
+            nav a:hover img,
+            nav a:focus img {
+              opacity: 0.3;
+            }
 
-          .color-yellow {
-            color: rgba(253, 239, 132, 1);
-          }
+            .color-yellow img {
+              height: 1em;
+            }
 
-          .color-red img {
-            height: 2em;
-          }
+            .color-yellow {
+              color: rgba(253, 239, 132, 1);
+            }
 
-          .color-red {
-            color: rgba(247, 198, 169, 1);
-          }
+            .color-red img {
+              height: 2em;
+            }
 
-          .color-blue img {
-            height: 1.2em;
-            margin-right: 0.2em;
-          }
+            .color-red {
+              color: rgba(247, 198, 169, 1);
+            }
 
-          .color-blue {
-            color: rgba(21, 186, 196, 1);
-          }
-          .color-grey {
-            color: #565656;
-          }
+            .color-blue img {
+              height: 1.2em;
+              margin-right: 0.2em;
+            }
 
-          p.color-grey {
-            margin: 0;
-          }
-        `}</style>
+            .color-blue {
+              color: rgba(21, 186, 196, 1);
+            }
+            .color-grey {
+              color: #565656;
+            }
+
+            p.color-grey {
+              margin: 0;
+            }
+          `}
+        </style>
         <style global jsx>
           {`
             body {
