56a384ffdf66178b62ee7708d02eb69843bdf39c
NeverEnder4 <apettigrew.wsdev@gmail.com>
2019-01-07 19:46:57 -0800

    add styles to demonstrations page

--- components/Demonstration/Demonstration.js
+++ components/Demonstration/Demonstration.js
@@ -1,124 +1,178 @@
-const Demonstration = ({ demo }) => {
-  const getColorClass = () => {
+export default class Demonstration extends React.Component {
+  constructor() {
+    super();
+    this.demoItemRef;
+    this.setDemoItemRef = element => (this.demoItemRef = element);
+  }
+  componentDidMount() {
+    this.props.timelinelite.to(
+      this.demoItemRef,
+      0.08,
+      { opacity: 1, x: '0%', ease: Expo.easeInOut },
+      '+=0.05',
+    );
+  }
+  getColorClass = () => {
     const i = Math.floor(Math.random() * 3);
     const colorClasses = ['color-red', 'color-yellow', 'color-blue'];
     return colorClasses[i];
   };
-  return (
-    <div className="display-box">
-      <div className="cover" />
-      <h1 className={getColorClass()}>{demo.title}</h1>
-      <div className="description">
-        <p dangerouslySetInnerHTML={{ __html: demo.content }} />
-        <div className="display-box__navigation">
-          <a className="github-nav" href={demo.metadata.github_repo}>
-            Github Repo
-          </a>
-          <a className="demo-nav" href={demo.metadata.link}>
-            Visit Site
-          </a>
+  render() {
+    const { demo } = this.props;
+    return (
+      <div
+        ref={element => this.setDemoItemRef(element)}
+        className="display-box"
+      >
+        <div className="cover" />
+        <h1 className={this.getColorClass()}>{demo.title}</h1>
+        <div className="description">
+          <h2>{demo.title}</h2>
+          <div
+            className="content"
+            dangerouslySetInnerHTML={{ __html: demo.content }}
+          />
+          <div className="display-box__navigation">
+            <a
+              className="demo-nav"
+              href={demo.metadata.github_repo}
+              target="_blank"
+            >
+              <img
+                src="/static/github-demo-link-icon.svg"
+                className="demo-nav__icon github"
+              />
+              <span className="link-text">Github Repo</span>
+            </a>
+            <a className="demo-nav" href={demo.metadata.link} target="_blank">
+              <img
+                src="/static/new-window-icon.svg"
+                className="demo-nav__icon demo"
+              />
+              <span className="link-text">Visit Site</span>
+            </a>
+          </div>
         </div>
+
+        <style jsx>{`
+          .display-box {
+            background: url(${demo.metadata.img.imgix_url});
+            background-size: cover;
+            background-repeat: no-repeat;
+            position: relative;
+            box-shadow: 0 4px 5px rgba(0, 0, 0, 0.3);
+            transition: all 300ms ease-in;
+            opacity: 0;
+            transform: translateX(100%);
+          }
+
+          .cover {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            left: 0;
+            top: 0;
+            background: rgba(0, 0, 0, 0.4);
+            z-index: 5;
+            transition: all 300ms ease-in;
+          }
+
+          h1 {
+            font-size: 4rem;
+            font-family: 'Righteous', sans-serif;
+            font-weight: 400;
+            text-transform: uppercase;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            z-index: 10;
+            transform: translate(-50%, -50%);
+            margin: 0;
+            text-align: center;
+            text-shadow: 0 0 2px rgba(0, 0, 0, 1);
+            transition: all 150ms ease-in;
+          }
+
+          .description {
+            position: absolute;
+            padding: 1em 2em;
+            z-index: 15;
+            transform: translateX(10%);
+            opacity: 0;
+            visibility: hidden;
+            transition: all 300ms ease-in-out;
+          }
+
+          .description h2 {
+            color: #565656;
+            margin-top: 3px;
+          }
+
+          .description div {
+            margin: 0;
+            color: #efefef;
+            line-height: 1.8;
+          }
+
+          .description img {
+            margin-right: 5px;
+            width: 1rem;
+          }
+
+          .demo-nav {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .display-box__navigation {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .link-text {
+            opacity: 0;
+            transform: translateX(-25%);
+            transition: all 300ms ease-in;
+            transition-delay: 100ms;
+          }
+
+          .github-nav,
+          .demo-nav {
+            color: #565656;
+            padding: 0.5em 1em;
+          }
+
+          .display-box-center:hover,
+          .display-box-center:focus {
+            background-position: center center;
+          }
+
+          .display-box:hover span {
+            opacity: 1;
+            transform: translateX(0);
+          }
+
+          .display-box:hover .cover,
+          .display-box:focus .cover {
+            background: rgba(0, 0, 0, 0.9);
+          }
+          .display-box:hover h1,
+          .display-box:focus h1 {
+            opacity: 0;
+            visibility: hidden;
+            transform: translate(-100%, -50%);
+          }
+
+          .display-box:hover .description,
+          .display-box:focus .description {
+            transform: translateX(0);
+            opacity: 1;
+            visibility: visible;
+          }
+        `}</style>
       </div>
-
-      <style jsx>{`
-        .display-box {
-          background: url(${demo.metadata.img.imgix_url});
-          background-size: cover;
-          background-repeat: no-repeat;
-          position: relative;
-          transition: all 300ms ease-in;
-        }
-
-        .cover {
-          position: absolute;
-          width: 100%;
-          height: 100%;
-          left: 0;
-          top: 0;
-          background: rgba(0, 0, 0, 0.4);
-          z-index: 5;
-          transition: all 300ms ease-in;
-        }
-
-        h1 {
-          font-size: 4rem;
-          font-family: 'Righteous', sans-serif;
-          font-weight: 400;
-          text-transform: uppercase;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          z-index: 10;
-          transform: translate(-50%, -50%);
-          margin: 0;
-          text-align: center;
-          text-shadow: 0 0 2px rgba(0, 0, 0, 1);
-          transition: all 150ms ease-in;
-        }
-
-        .description {
-          position: absolute;
-          padding: 1em 2em;
-          z-index: 15;
-          transform: translateX(10%);
-          opacity: 0;
-          transition: all 300ms ease-in-out;
-        }
-
-        .description p {
-          margin: 0;
-          color: #efefef;
-        }
-
-        .display-box__navigation {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          padding: 0 2em;
-          padding-top: 1em;
-        }
-
-        .github-nav {
-          color: rgba(247, 198, 169, 1);
-          border: 1px solid rgba(247, 198, 169, 1);
-          margin-right: 1em;
-        }
-
-        .demo-nav {
-          color: rgba(21, 186, 196, 1);
-          border: 1px solid rgba(21, 186, 196, 1);
-        }
-
-        .github-nav,
-        .demo-nav {
-          padding: 0.5em 1em;
-          border-radius: 10px;
-        }
-
-        .display-box-center:hover,
-        .display-box-center:focus {
-          background-position: center center;
-        }
-
-        .display-box:hover .cover,
-        .display-box:focus .cover {
-          background: rgba(0, 0, 0, 0.9);
-        }
-        .display-box:hover h1,
-        .display-box:focus h1 {
-          opacity: 0;
-          visibility: hidden;
-          transform: translate(-100%, -50%);
-        }
-
-        .display-box:hover .description,
-        .display-box:focus .description {
-          transform: translateX(0);
-          opacity: 1;
-        }
-      `}</style>
-    </div>
-  );
-};
-
-export default Demonstration;
+    );
+  }
+}
--- pages/demonstrations.js
+++ pages/demonstrations.js
@@ -1,9 +1,16 @@
 import fetch from 'isomorphic-unfetch';
 import Error from 'next/error';
+import { TimelineLite } from 'gsap';
+
 import Layout from '../components/Layout';
 import Demonstration from '../components/Demonstration/Demonstration';
 
 export default class demonstrations extends React.Component {
+  constructor() {
+    super();
+
+    this.tll = new TimelineLite({ delay: 0.2 });
+  }
   static async getInitialProps() {
     let demonstrations;
     try {
@@ -16,6 +23,7 @@ export default class demonstrations extends React.Component {
     }
     return { demonstrations };
   }
+
   render() {
     const demonstrations = this.props.demonstrations.objectsByType;
     if (!demonstrations.length) return <Error statusCode={503} />;
@@ -25,7 +33,11 @@ export default class demonstrations extends React.Component {
           <div className="container">
             <div className="demonstrations-grid">
               {demonstrations.map(demo => (
-                <Demonstration key={demo._id} demo={demo} />
+                <Demonstration
+                  timelinelite={this.tll}
+                  key={demo._id}
+                  demo={demo}
+                />
               ))}
             </div>
           </div>
@@ -42,6 +54,8 @@ export default class demonstrations extends React.Component {
               display: grid;
               height: 60vh;
               grid-template-columns: repeat(3, 1fr);
+              grid-template-columns: repeat(3, 1fr);
+
               grid-gap: 20px;
               padding: 10em;
             }
